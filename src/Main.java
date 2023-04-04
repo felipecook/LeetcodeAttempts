a508a95009919fe5bd157147ceeac1f252d076a0
Felipe Cook <FelipepCook@gmail.com>
2023-04-04 21:32:03 +0800

    isPalindrome beginning of solution https://leetcode.com/problems/palindrome-number/

--- src/Main.java
+++ src/Main.java
@@ -31,29 +31,45 @@ public class Main {
         return intArrayReturn;
     }
 
-    public static int[] newMethod(int[] nums, int target){
-        Map hm = new HashMap();
-        for(int i=0; i {
-            hm.put(nums[i],i);
+//    public static int[] newMethod(int[] nums, int target){
+//        Map hm = new HashMap();
+//        for(int i=0; i {
+//            hm.put(nums[i],i);
+//        }
+//        for(int i=0; i {
+//            int k =nums[i];
+//            if(k == target && hm.containsKey(0) )
+//            {
+//                arr[0]= i;
+//                arr[1]=hm.get(0);
+//                break;
+//            }
+//            else if(hm.containsKey(target-k) )
+//            {
+//                if(hm.get(target-k)>i)
+//                {      arr[0]= i;
+//                    arr[1]=hm.get(target-k);
+//                    break;}
+//            }
+//        }
+//        return arr;
+//    }
+
+    public static boolean isPalindrome(int x) {
+        String xAsString = Integer.toString(x);
+        if (x < 0) {
+            return false;
         }
-        for(int i=0; i {
-            int k =nums[i];
-            if(k == target && hm.containsKey(0) )
-            {
-                arr[0]= i;
-                arr[1]=hm.get(0);
-                break;
-            }
-            else if(hm.containsKey(target-k) )
-            {
-                if(hm.get(target-k)>i)
-                {      arr[0]= i;
-                    arr[1]=hm.get(target-k);
-                    break;}
-            }
+        if (x % 10 == 0  && x != 0){
+            return false;
         }
-        return arr;
+        for (int i = 0; i < xAsString.length() / 2; i++){
+            String charHolder = Character.toString(xAsString.charAt(i));
+
+        }
+
+        return true;
     }
-    }
+
 
 }
